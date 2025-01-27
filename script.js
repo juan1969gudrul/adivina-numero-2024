ebef61637379831a373f8327ced0d5d2cd514aa1
Juan <Jjangulo91@gmail.com>
2025-01-27 12:12:37 +0100

    añadidos comentarios

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict'
 const INITIAL_SCORE = 20
 const MAX_NUMBER = 20
+
 function initData() {
   let score = INITIAL_SCORE
   let highscore = 0
   let number = Math.trunc(Math.random() * MAX_NUMBER) + 1
 }
+
 /*seleccionar todos los elementos del DOM que necesitamos*/
 const messageField = document.querySelector('.message')
 const scoreField = document.querySelector('.score')
@@ -14,18 +16,22 @@ const numberField = document.querySelector('.number')
 const guessField = document.querySelector('.guess')
 const checkButton = document.querySelector('.check')
 const againButton = document.querySelector('.again')
+
 console.log(scoreField)
 console.log(scoreField.textContent)
 console.log(highscoreField)
 console.log(highscoreField.textContent)
+
 //eventos
 checkButton.addEventListener('click', checkNumber)
 againButton.addEventListener('click', initData)
+
 function checkNumber() {
-  console.log('ahora comprobaríamos el número')
-//obtenemos el número pulsado
-//si no es un número, que lo corrija
-//si es un número y es correcto... ---> comprobamos score ¿perdemos partida?
-//actualizamos nuestras variables y el DOM
-//si es un número y es incorrecto...
+  console.log('ahora comprobaríamos el número')
+  //obtenemos el número pulsado
+  //si no es un número, que lo corrija
+  //si es un número y es correcto... ---> comprobamos score ¿perdemos partida?
+  //actualizamos nuestras variables y el DOM
+
+  //si es un número y es incorrecto...
 }
